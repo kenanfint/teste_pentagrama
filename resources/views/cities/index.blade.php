1c626ebff5e9aabc26b442de34b0ce67e323147d
Kenan Fintelman <kenanfintelman123@hotmail.com>
2022-09-24 17:10:46 -0300

    feat: creating cities table

---
+++ resources/views/cities/index.blade.php
@@ -0,0 +1,360 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Tabela de Cidades</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<link href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" rel="stylesheet" type="text/css">
+	<style>
+		body {
+			color: #566787;
+			background: #f5f5f5;
+			font-family: 'Varela Round', sans-serif;
+			font-size: 13px;
+		}
+
+		.table-responsive {
+			margin: 30px 0;
+		}
+
+		.table-wrapper {
+			background: #fff;
+			padding: 20px 25px;
+			border-radius: 3px;
+			min-width: 1000px;
+			box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
+		}
+
+		.table-title {
+			padding-bottom: 15px;
+			background: #435d7d;
+			color: #fff;
+			padding: 16px 30px;
+			min-width: 100%;
+			margin: -20px -25px 10px;
+			border-radius: 3px 3px 0 0;
+		}
+
+		.table-title h2 {
+			margin: 5px 0 0;
+			font-size: 24px;
+		}
+
+		.table-title .btn-group {
+			float: right;
+		}
+
+		.table-title .btn {
+			color: #fff;
+			float: right;
+			font-size: 13px;
+			border: none;
+			min-width: 50px;
+			border-radius: 2px;
+			border: none;
+			outline: none !important;
+			margin-left: 10px;
+		}
+
+		.table-title .btn i {
+			float: left;
+			font-size: 21px;
+			margin-right: 5px;
+		}
+
+		.table-title .btn span {
+			float: left;
+			margin-top: 2px;
+		}
+
+		table.table tr th,
+		table.table tr td {
+			border-color: #e9e9e9;
+			padding: 12px 15px;
+			vertical-align: middle;
+		}
+
+		table.table tr th:first-child {
+			width: 60px;
+		}
+
+		table.table tr th:last-child {
+			width: 100px;
+		}
+
+		table.table-striped tbody tr:nth-of-type(odd) {
+			background-color: #fcfcfc;
+		}
+
+		table.table-striped.table-hover tbody tr:hover {
+			background: #f5f5f5;
+		}
+
+		table.table th i {
+			font-size: 13px;
+			margin: 0 5px;
+			cursor: pointer;
+		}
+
+		table.table td:last-child i {
+			opacity: 0.9;
+			font-size: 22px;
+			margin: 0 5px;
+		}
+
+		table.table td a {
+			font-weight: bold;
+			color: #566787;
+			display: inline-block;
+			text-decoration: none;
+			outline: none !important;
+		}
+
+		table.table td a:hover {
+			color: #2196F3;
+		}
+
+		table.table td a.edit {
+			color: #FFC107;
+		}
+
+		table.table td a.delete {
+			color: #F44336;
+		}
+
+		table.table td i {
+			font-size: 19px;
+		}
+
+		table.table .avatar {
+			border-radius: 50%;
+			vertical-align: middle;
+			margin-right: 10px;
+		}
+
+		.pagination {
+			float: right;
+			margin: 0 0 5px;
+		}
+
+		.pagination li a {
+			border: none;
+			font-size: 13px;
+			min-width: 30px;
+			min-height: 30px;
+			color: #999;
+			margin: 0 2px;
+			line-height: 30px;
+			border-radius: 2px !important;
+			text-align: center;
+			padding: 0 6px;
+		}
+
+		.pagination li a:hover {
+			color: #666;
+		}
+
+		.pagination li.active a,
+		.pagination li.active a.page-link {
+			background: #03A9F4;
+		}
+
+		.pagination li.active a:hover {
+			background: #0397d6;
+		}
+
+		.pagination li.disabled i {
+			color: #ccc;
+		}
+
+		.pagination li i {
+			font-size: 16px;
+			padding-top: 6px
+		}
+
+		.hint-text {
+			float: left;
+			margin-top: 10px;
+			font-size: 13px;
+		}
+
+		/* Modal styles */
+		.modal .modal-dialog {
+			max-width: 400px;
+		}
+
+		.modal .modal-header,
+		.modal .modal-body,
+		.modal .modal-footer {
+			padding: 20px 30px;
+		}
+
+		.modal .modal-content {
+			border-radius: 3px;
+			font-size: 14px;
+		}
+
+		.modal .modal-footer {
+			background: #ecf0f1;
+			border-radius: 0 0 3px 3px;
+		}
+
+		.modal .modal-title {
+			display: inline-block;
+		}
+
+		.modal .form-control {
+			border-radius: 2px;
+			box-shadow: none;
+			border-color: #dddddd;
+		}
+
+		.modal textarea.form-control {
+			resize: vertical;
+		}
+
+		.modal .btn {
+			border-radius: 2px;
+			min-width: 100px;
+		}
+
+		.modal form label {
+			font-weight: normal;
+		}
+	</style>
+	<script>
+		$(document).ready(function() {
+			// Activate tooltip
+			$('[data-toggle="tooltip"]').tooltip();
+
+			// Select/Deselect checkboxes
+			var checkbox = $('table tbody input[type="checkbox"]');
+			$("#selectAll").click(function() {
+				if (this.checked) {
+					checkbox.each(function() {
+						this.checked = true;
+					});
+				} else {
+					checkbox.each(function() {
+						this.checked = false;
+					});
+				}
+			});
+			checkbox.click(function() {
+				if (!this.checked) {
+					$("#selectAll").prop("checked", false);
+				}
+			});
+		});
+	</script>
+</head>
+
+<body>
+	<div class="container-xl">
+		<div class="table-responsive">
+			<form action="{{ route('cities.filter') }}" class="d-flex justify-content-center pb-2" method="post">
+				@csrf
+				<input type="text" class="form-control w-100" placeholder="Digite o nome da cidade" name="city_name">
+				<input type="date" maxlength="10" class='form-control w-100' name="foundation_date" value="" placeholder="Insira a data">
+				<input type="text" class="form-control w-100" placeholder="Digite o nome do bairro" name="district">
+				<button onClick="submit" class="btn btn-primary ml-1">filtrar</button>
+			</form>
+
+			<div class="table-wrapper">
+				<div class="table-title">
+					<div class="row">
+						<div class="col-sm-6">
+							<h2>Gerenciar <b>Endereços</b></h2>
+						</div>
+						<div class="col-sm-6">
+							<a href="#addCity" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Adicionar Cidade</span></a>
+						</div>
+					</div>
+				</div>
+				<table class="table table-striped table-hover">
+					<thead>
+						<tr>
+							<th>
+								
+							
+							</th>
+							<th>Cidade</th>
+							<th>Estado</th>
+							<th>Fundação</th>
+							<th>Bairro</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach ($cities as $city)
+						<tr>
+							<td>
+								
+							</td>
+							<td>{{$city->name}}</td>
+							<td>{{$city->state}}</td>
+							<td>{{ date('d/m/Y', strtotime($city->foundation_date)) }}</td>
+							<td>{{$city->district->name}}</td>
+							
+						</tr>
+						@endforeach
+
+
+					</tbody>
+				</table>
+				<div class="clearfix">
+					<ul class="pagination">
+						{{$cities->links("pagination::bootstrap-4")}}
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Add Modal HTML -->
+	<div id="addCity" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<form action="{{ route('cities.store')}}">
+					@csrf
+					<div class="modal-header">
+						<h4 class="modal-title">Adicionar Cidade</h4>
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					</div>
+					<div class="modal-body">
+						<div class="form-group">
+							<label>Cidade</label>
+							<input type="text" class="form-control" name="city_name" placeholder="Ex: São Paulo" required>
+						</div>
+						<div class="form-group">
+							<label>Estado</label>
+							<input type="text" class="form-control" name="state_name" required>
+						</div>
+						<div class="form-group">
+							<label for="date">Data de fundação</label>
+							<input type="date" class='form-control' name="foundation_date" value="" placeholder="insira a data" required>
+						</div>
+						<div class="form-group">
+							<label>Bairro</label>
+							<input type="text" class="form-control" name="district_name" required>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
+						<input type="submit" class="btn btn-success" value="Adicionar">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
