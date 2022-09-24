aa399fe655a35f4c301dcdcbed41863054a18a74
Kenan Fintelman <kenanfintelman123@hotmail.com>
2022-09-24 17:09:59 -0300

    feat: creating user register view

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -78,14 +78,7 @@
                         <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
                         <label for="floatingPassword">Senha</label>
                       </div>
-      
-                      <div class="form-check mb-3">
-                        <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck">
-                        <label class="form-check-label" for="rememberPasswordCheck">
-                          Lembrar senha
-                        </label>
-                      </div>
-      
+
                       <div class="d-grid">
                         <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="submit">Registrar</button>
                         <div class="text-center">
