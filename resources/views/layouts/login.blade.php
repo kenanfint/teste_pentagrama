e95d84763bf2d68f19789f31ae789ba5dc1237bc
Kenan Fintelman <kenanfintelman123@hotmail.com>
2022-09-25 14:25:07 -0300

    refactor: refactoring views

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+
+    <style>
+        .login {
+            min-height: 100vh;
+        }
+
+        .bg-image {
+            background-image: url('https://source.unsplash.com/WEQbe2jBg40/600x1200');
+            background-size: cover;
+            background-position: center;
+        }
+
+        .login-heading {
+            font-weight: 300;
+        }
+
+        .btn-login {
+            font-size: 0.9rem;
+            letter-spacing: 0.05rem;
+            padding: 0.75rem 1rem;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="app">
+        @yield('content')
+    </div>
+</body>
+
+</html>
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,103 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+@extends('layouts.login')
 
-    <style>
-        .login {
-            min-height: 100vh;
-            }
-    
-        .bg-image {
-            background-image: url('https://source.unsplash.com/WEQbe2jBg40/600x1200');
-            background-size: cover;
-            background-position: center;
-            }
-    
-        .login-heading {
-            font-weight: 300;
-            }
-    
-        .btn-login {
-            font-size: 0.9rem;
-            letter-spacing: 0.05rem;
-            padding: 0.75rem 1rem;
-        }
-    
-    </style>
-</head>
-<body>
-  
-    <div class="container-fluid ps-md-0">
-        <div class="row g-0">
-          <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
-          <div class="col-md-8 col-lg-6">
-            <div class="login d-flex align-items-center py-5">
-              <div class="container">
-                <div class="row">
-                  <div class="col-md-9 col-lg-8 mx-auto">
+@section('title')
+Registrar-se
+@endsection
 
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
+@section('content')
 
-                    @if(session('danger'))
-                        <div class="alert alert-danger">
-                            {{ session('danger') }}
-                        </div>
-                    @endif
+<div class="container-fluid ps-md-0">
+  <div class="row g-0">
+    <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+    <div class="col-md-8 col-lg-6">
+      <div class="login d-flex align-items-center py-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-9 col-lg-8 mx-auto">
 
-                    <h3 class="login-heading mb-4">Crie sua conta!</h3>
-      
-                    <!-- Sign In Form -->
-                    <form action="{{ route('users.store') }}" method="post">
-                      @csrf
-                      <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingInput" name="username">
-                        <label for="floatingInput">Nome</label>
-                      </div>
+              @if ($errors->any())
+              <div class="alert alert-danger">
+                <ul>
+                  @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                  @endforeach
+                </ul>
+              </div>
+              @endif
 
-                      <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email">
-                        <label for="floatingInput">Email</label>
-                      </div>
-                      <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
-                        <label for="floatingPassword">Senha</label>
-                      </div>
+              @if(session('danger'))
+              <div class="alert alert-danger">
+                {{ session('danger') }}
+              </div>
+              @endif
 
-                      <div class="d-grid">
-                        <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="submit">Registrar</button>
-                        <div class="text-center">
-                          <a class="small" href="/">Esqueceu a senha?</a>
-                        </div>
-                      </div>
-                    </form>
-                    <div class="d-grid pt-5">
-                      <div class="text-center">
-                        <a class="small" href="{{ route('users.index') }}">Fazer login</a>
-                      </div>
-                    </div>
+              <h3 class="login-heading mb-4">Crie sua conta!</h3>
+
+              <!-- Sign In Form -->
+              <form action="{{ route('users.store') }}" method="post">
+                @csrf
+                <div class="form-floating mb-3">
+                  <input type="text" class="form-control" id="floatingInput" name="username">
+                  <label for="floatingInput">Nome</label>
+                </div>
+
+                <div class="form-floating mb-3">
+                  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email">
+                  <label for="floatingInput">Email</label>
+                </div>
+                <div class="form-floating mb-3">
+                  <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
+                  <label for="floatingPassword">Senha</label>
+                </div>
+
+                <div class="d-grid">
+                  <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="submit">Registrar</button>
+                  <div class="text-center">
+                    <a class="small" href="{{ route('users.login') }}">Fazer login</a>
                   </div>
                 </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
-</body>
-</html>
-  
+    </div>
+  </div>
+</div>
+
+@endsection
